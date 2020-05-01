9cd23b671367dfceb100f016bdbe0f27bde2ed23
mchirsydd <mchirsyd@yahoo.co.id>
2020-05-01 21:21:22 +0700

    starter project

---
+++ app/src/main/java/com/lazday/covid_19/MainModel.kt
@@ -0,0 +1,8 @@
+package com.lazday.covid_19
+
+data class MainModel (
+    val name: String,
+    val positif: String,
+    val sembuh: String,
+    val meninggal: String
+)
---
+++ app/src/main/java/com/lazday/covid_19/retrofit/ApiEndpoint.kt
@@ -0,0 +1,11 @@
+package com.lazday.covid_19.retrofit
+
+import com.lazday.covid_19.MainModel
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiEndpoint {
+
+    @GET("agen")
+    fun dataIndonesia(): Call<MainModel>
+}
