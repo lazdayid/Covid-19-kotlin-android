81707c00aa57c568e5a2b56bf9f6481748eb2cd0
mchirsydd <mchirsyd@yahoo.co.id>
2020-05-01 21:57:24 +0700

    update endpoint

--- app/src/main/java/com/lazday/covid_19/MainModel.kt
+++ app/src/main/java/com/lazday/covid_19/MainModel.kt
@@ -1,10 +1,4 @@
 package com.lazday.covid_19
 
-import com.google.gson.annotations.SerializedName
-
-data class MainModel (
-    @SerializedName("name") val name: String,
-    @SerializedName("positif") val positif: String,
-    @SerializedName("sembuh") val sembuh: String,
-    @SerializedName("meninggal") val meninggal: String
-)
+data class MainModel ( val name: String, val positif: String, val sembuh: String,
+                       val meninggal: String )
