161fb3871e3d715034344a9b9ef27dca85e8f05f
mchirsydd <mchirsyd@yahoo.co.id>
2020-05-14 00:46:09 +0700

    update hello world

--- app/src/main/java/com/lazday/covid_19/MainActivity.kt
+++ app/src/main/java/com/lazday/covid_19/MainActivity.kt
@@ -2,12 +2,16 @@ package com.lazday.covid_19
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private val TAG: String = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "Hello world!")
     }
 
 }
